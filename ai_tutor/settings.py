094392e41f9e64912415a63b065347d9c4395667
NaveenRoman <naveenrom232@gmail.com>
2026-02-01 17:51:07 +0530

    Fix email environment variables

--- ai_tutor/settings.py
+++ ai_tutor/settings.py
@@ -137,15 +137,20 @@ LOGOUT_REDIRECT_URL = "/"
 # EMAIL (Production Safe)
 # ===============================
 
+# ===============================
+# EMAIL (Production Safe)
+# ===============================
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.environ.get("naveenrom232@gmail.com")
-EMAIL_HOST_PASSWORD = os.environ.get("sonfytayyatfshui" )
+EMAIL_HOST_PASSWORD = os.environ.get("sonfytayyatfshui")
 
 DEFAULT_FROM_EMAIL = f"KnowledgeStream <{EMAIL_HOST_USER}>"
+
 
 
 
